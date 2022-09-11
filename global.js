5bfc89bf48a0fed2f9472d63e389d9e0bf7e60d8
NotTheRightGuy <janmejaychatterjee@gmail.com>
2022-09-11 20:54:09 +0530

    Never gonna give you up bitches

--- global.js
+++ global.js
@@ -1,2 +1,18 @@
 let startButton = document.getElementById('start-button');
-startButton.onclick = alert('Hello World');
+let rickRoll = document.getElementById('rick-roll');
+
+counter = 0
+
+startButton.onclick = () =>{
+    if(counter  % 2 == 0){
+        rickRoll.style.display = 'block';
+        document.getElementById('header').innerHTML = "NEVER GONNA GIVE YOU UP";
+        startButton.innerHTML = 'NEVER GONNA LET YOU DOWN!!!'
+    }
+    else{
+        rickRoll.style.display = 'none';
+        document.getElementById('header').innerHTML = "Black Jack";
+        startButton.innerHTML = 'Start'
+    }
+    counter += 1;
+}
